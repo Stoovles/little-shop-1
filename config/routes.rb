4b9f572f09fba8a33b48e327433fa0c61c8b667e
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-04-02 16:59:55 -0600

    Adds logout functionality to nav bar and user logout functionality.
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>
    Co-authored-by: Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
    Co-authored-by: Ethan Grab <40487417+Stoovles@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
 
+  get '/logout', to: "sessions#destroy"
+
   get '/cart', to: "carts#show"
 
 
