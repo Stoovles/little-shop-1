8923c564a6fe1f835fb09220337eaecc9c9ea4aa
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-02 16:42:46 -0600

    Creates merchant_name method for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
+
+  def merchant_name
+    User.where(id: self.user_id).first.name
+  end
 end
