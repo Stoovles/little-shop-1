bc16e2bac388da65ede4f09a6cd37c65599401ac
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-02 08:05:11 -0600

    Adds test for admin visiting item index

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -7,11 +7,30 @@ RSpec.describe "Items Index Page", type: :feature do
     @u1 = User.create((name: "Con Chilver",address: "16455 Miller Circle",city: "Van Nuys",state: "California",zipcode: 91406,email: "cchilver2@mysql.com",password_digest:"IrGmrINsmr9e")
 
     @i1 = @umerch.items.create(item_name: "W.L. Weller Special Reserve",image: "http://www.buffalotracedistillery.com/sites/default/files/Weller_CYPB_750ml_front_LoRes.png",price: 20.0,inventory: 4, description:"A sweet nose with a presence of caramel. Tasting notes of honey, butterscotch, and a soft woodiness. It's smooth, delicate and calm. Features a smooth finish with a sweet honeysuckle flair.",enabled: true)
+
     @i2 = @umerch.items.create(item_name: "W.L. Weller C.Y.P.B.",image: "http://www.buffalotracedistillery.com/sites/default/files/weller%20special%20reserve%20brand%20page%5B1%5D.png",current_price: 35.0,inventory: 30, description:"A light aroma with citrus and oak on the nose. The palate is well rounded and balanced, with a medium-long finish and hints of vanilla.",enabled: false)
   end
 
   context "anyone visiting item catalog" do
-    it "shows all enabled items and their info" do
+    it "shows all enabled items and their info to a visitor" do
+      visit items_path
+      within first ".item-card" do
+        expect(page).to have_content(@i1.item_name)
+        expect(page).to have_css("img[src*='#{@i1.image}']")
+        expect(page).to have_content("Merchant: #{@i1.user.name}")
+        expect(page).to have_content("In inventory: :#{@i1.inventory}")
+        expect(page).to have_content("Price: $#{@i1.current_price}")
+      end
+      expect(page).to have_css(".item-card",count: 1)
+      expect(page).to_not have_content(@i2.item_name)
+    end
+
+    it "shows all enabled items and their info to an admin" do
+      visit root_path
+      click_link "Log in"
+      fill_in "Email", with: @uadmin.email
+      fill_in "Password", with: @uadmin.password
+      click_button "Log in"
       visit items_path
       within first ".item-card" do
         expect(page).to have_content(@i1.item_name)
