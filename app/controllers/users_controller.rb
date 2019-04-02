4c90def19d9dff4379981b3f3cf9df328c7ba0b7
Ethan Grab <ethangrab@gmail.com>
2019-04-01 18:43:57 -0600

    User Story 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "You are now registered and logged in"
-      redirect_to @user
+      redirect_to @user, success: "You are now registered and logged in"
+    elsif @user.exists?(email_address: user_params[:email_address])
+      render 'new', danger: "This email address already exists"
     else
-      render 'new'
+      render 'new', danger: "You are missing required fields"
     end
   end
 
