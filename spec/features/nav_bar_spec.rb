93949634eafde38925151c9870c1295010b20261
Matt Weiss <weiss.matt@outlook.com>
2019-04-02 20:37:36 -0600

    Completes User Story 3
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>
    Co-authored-by: Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
    Co-authored-by: Ethan Grab <40487417+Stoovles@users.noreply.github.com>

--- spec/features/nav_bar_spec.rb
+++ spec/features/nav_bar_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'When I visit our application I see a navbar' do
+  before :each do
+    @user = User.create(name: "Matt",
+                      role: 0,
+                      street_address: "1331 17th St",
+                      city: "Denver",
+                      state: "CO",
+                      zip_code: 80202,
+                      email_address: "MattW@test_user.com",
+                      password: "test",
+                      enabled: true)
+    end
+
   #User Story 2
   describe 'As an unregistered user I see' do
     it 'has links to features available to unregistered users' do
@@ -41,30 +53,51 @@ RSpec.describe 'When I visit our application I see a navbar' do
     end
   end
 
-#   #User Story 3
-#   describe 'As a registered user I see' do
-#     @user = User.create(first_name: 'Matt', last_name: 'Weiss')
-#     # login user here
-#     within(".navbar") do
-#       xit 'has all the above links except login and register' do
-#         expect(page).to have_link 'Home Page' #root_path
-#         expect(page).to have_link 'All Items', items_path
-#         expect(page).to have_link 'Merchants', merchants_path
-#         expect(page).to have_link 'My Cart', cart_path(my_cart)
-#         expect(page).to_not have_link 'Log In' #login_path
-#         expect(page).to_not have_link 'Register' #register_path
-#       end
-#
-#       xit 'displays the user name' do
-#         expect(page).to have_content 'Welcome Back, Matt'
-#       end
-#
-#       xit 'also has logout and profile links' do
-#         expect(page).to have_link 'Log Out' #logout_path
-#         expect(page).to have_link 'Profile', user_path(@user)
-#       end
-#     end
-#   end
+  #User Story 3
+  describe 'As a registered user I see' do
+
+    it 'has all the above links except login and register and also has logout and profile links' do
+      visit root_path
+      within(".navbar") do
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        expect(page).to have_link 'Home'
+        expect(current_path).to eq(root_path)
+
+        expect(page).to have_link 'All Items'
+        click_link 'All Items'
+        expect(current_path).to eq(items_path)
+
+        expect(page).to have_link 'Merchants'
+        click_link 'Merchants'
+        expect(current_path).to eq(merchants_path)
+
+        expect(page).to have_link 'My Cart'
+        click_link 'My Cart'
+        expect(current_path).to eq(cart_path)
+
+        expect(page).to have_link 'Profile'
+        click_link 'Profile'
+        expect(current_path).to eq(user_path(@user))
+
+        expect(page).to have_link 'Log Out'
+
+        expect(page).to_not have_link 'Log In'
+        expect(page).to_not have_link 'Register'
+      end
+    end
+
+    it 'displays the user name' do
+      visit root_path
+      within(".navbar") do
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        visit root_path
+        expect(page).to have_content 'Logged in as Matt'
+      end
+    end
+  end
+end
 #
 #   #User Story 4
 #   describe 'As a merchant user I see' do
@@ -109,4 +142,4 @@ RSpec.describe 'When I visit our application I see a navbar' do
 #       end
 #     end
 #   end
-end
+# end
