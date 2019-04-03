d622765364123e9979e7c58fee3d261cc86266fa
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-04-02 22:28:55 -0600

    adds test and show functionality for profile info and has link to edit

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -8,22 +8,25 @@ RSpec.describe 'As a registered user' do
                       state: "CO",
                       zip_code: 80202,
                       email_address: "Jeremy@test_user.com",
-                      password: "test",
+                      password: "password",
                       enabled: true)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
-  describe 'when I visit my own profile page'
+  describe 'when I visit my own profile page' do
     it "I can see all my profile data except my password" do
-      
-      visit user_path(@user)
 
-      expect(page).to have_content("Name: #{@user.name}")
-      expect(page).to have_content("Street Address: #{@user.street_address}")
-      expect(page).to have_content("City: #{@user.city}")
-      expect(page).to have_content("State: #{@user.state}")
-      expect(page).to have_content("Zip Code: #{@user.zip_code}")
-      expect(page).to have_content("Email Address: #{@user.email_address}")
-      expect(page).to_not have_content("#{@user.password}")
+      visit user_path(@user)
+      within "#profile-info" do
+        expect(page).to have_content("Name: #{@user.name}")
+        expect(page).to have_content("Street Address: #{@user.street_address}")
+        expect(page).to have_content("City: #{@user.city}")
+        expect(page).to have_content("State: #{@user.state}")
+        expect(page).to have_content("Zip Code: #{@user.zip_code}")
+        expect(page).to have_content("Email Address: #{@user.email_address}")
+        expect(page).to_not have_content("#{@user.password}")
+      end
+      expect(page).to have_link("Edit my profile")
     end
+  end
 end
