8578f799180d383a2c8d804fc10f91ae309cff1b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-03 14:41:46 -0600

    Adds cart class and model tests
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>
    Co-authored-by: Ethan Grab <40487417+Stoovles@users.noreply.github.com>
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

---
+++ app/models/cart.rb
@@ -0,0 +1,21 @@
+class Cart
+  attr_reader :contents
+
+
+  def initialize(contents)
+    @contents = contents || Hash.new(0)
+  end
+
+  def total_count
+    @contents.values.sum if @contents
+  end
+
+  def count_of(item_id)
+    @contents[item_id.to_s]
+  end
+
+  def add_item(item_id)
+    @contents[item_id.to_s] += 1
+  end
+
+end
