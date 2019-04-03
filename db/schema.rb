c5d463d40057f671d144a199a6e79d58bb1ec0cb
Ethan Grab <ethangrab@gmail.com>
2019-04-03 14:07:08 -0600

    default user role migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190402232927) do
+ActiveRecord::Schema.define(version: 20190403200156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20190402232927) do
     t.string "zip_code"
     t.string "email_address"
     t.string "password_digest"
-    t.integer "role"
+    t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "enabled"
