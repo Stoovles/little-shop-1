eb916c807ce43d634afa8bfbc479157672e508c3
Matt Weiss <weiss.matt@outlook.com>
2019-04-04 13:33:03 -0600

    Finishes User Story 23
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>
    Co-authored-by: Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,6 +11,11 @@ before_action :require_visitor_or_user
     redirect_to item_path(params[:item_id]), info: "You now have #{session[:cart][item.id.to_s]} #{item.item_name} in your cart."
   end
 
+  def destroy
+    session.delete(:cart)
+    redirect_to cart_path
+  end
+
 private
   def require_visitor_or_user
     render file: "/public/404" unless current_user? || !current_user
