08d90b861b95d724e132665a7e325301d23146e3
Ethan Grab <ethangrab@gmail.com>
2019-04-05 06:33:30 -0600

    Merchant pending order card - show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,10 @@ skip_before_action :require_user, only: [:new, :create]
   end
 
   def create
-
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      @pending_merchant_items = OrderItem.unfulfilled_merchant_orderitems
       redirect_to profile_path, success: "You are now registered and logged in"
     else
       render :new
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,4 +5,9 @@ class OrderItem < ApplicationRecord
   def fulfilled?
     self.fulfilled
   end
+
+  def self.unfulfilled_merchant_orderitems
+    all.where(item_id: [current_user.items.pluck(:id)]).where(fulfilled: false)
+  end
+
 end
