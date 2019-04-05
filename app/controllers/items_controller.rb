71b9fe95fd25ad03be7d11cdb0c6c032092b60d6
Ethan Grab <ethangrab@gmail.com>
2019-04-05 14:29:38 -0600

    Added determine scope to allow for /items and dashboard/items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :determine_scope
 
   def index
-    @items = Item.all.where(enabled: true)
+    @items = @scope.all
   end
 
   def show
     @item = Item.find(params[:id])
   end
+
+  protected
+
+   def determine_scope
+     @scope = if request.env['PATH_INFO'] == "/dashboard/items"
+       Item.merchant_items(current_user)
+     else
+       Item.all.where(enabled: true)
+     end
+   end
 
 end
