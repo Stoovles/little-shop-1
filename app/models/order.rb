83bc899a8444601b7588d06d00e4f2ac3a648ccb
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-04 18:09:04 -0600

    Adds item_quantity method to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :items, through: :order_items
   belongs_to :user
+
+  def item_quantity
+    OrderItem.where(order_id: self.id).sum(:quantity)
+  end
+
 end
