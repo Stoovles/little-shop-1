2ea10a678c7f5967ff6be6936d132ff130c27178
Ethan Grab <ethangrab@gmail.com>
2019-04-06 16:15:02 -0600

    Proper format for flash messages

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -8,15 +8,13 @@ class Dashboard::ItemsController < ApplicationController
   def deactivate
   @item = Item.find(params[:id])
   @item.update(enabled: false)
-  flash.notice = "Item #{@item.id} has been disabled"
-  redirect_to dashboard_items_path
+  redirect_to dashboard_items_path,  danger: "Item #{@item.id} has been disabled"
 end
 
 def activate
   @item = Item.find(params[:id])
   @item.update(enabled: true)
-  flash.notice = "Item #{@item.id} has been enabled"
-  redirect_to dashboard_items_path
+  redirect_to dashboard_items_path, success: "Item #{@item.id} has been enabled"
 end
 
   private
