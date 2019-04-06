a0f5eac91a8738d58b2fe4bc037b00c9604d17b2
Matt Weiss <weiss.matt@outlook.com>
2019-04-06 14:47:32 -0600

    Completes user story 44

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -1,5 +1,13 @@
 class Dashboard::ItemsController < ApplicationController
+  before_action :require_merchant
+
   def index
     @items = Item.merchant_items(current_user)
+  end
+
+  private
+
+  def require_merchant
+    render file: "/public/404" unless current_merchant?
   end
 end
