4774e3880ffe0248121d90cc3269dbc25309ca18
Ethan Grab <ethangrab@gmail.com>
2019-04-07 06:45:19 -0600

    Create and new methods as well as private item_params

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -1,6 +1,21 @@
 class Dashboard::ItemsController < ApplicationController
   before_action :require_merchant
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    # item_params[:image_url] ||= #default image
+    @item = Item.new(item_params)
+    if @item.save
+      session[:item_id] = @item.id
+      redirect_to dashboard_items_path, success: "Item #{@item.id} has been created"
+    else
+      render :new
+    end
+  end
+
   def index
     @items = Item.merchant_items(current_user)
   end
@@ -11,16 +26,16 @@ class Dashboard::ItemsController < ApplicationController
   end
 
   def deactivate
-  @item = Item.find(params[:id])
-  @item.update(enabled: false)
-  redirect_to dashboard_items_path,  danger: "Item #{@item.id} has been disabled"
-end
+    @item = Item.find(params[:id])
+    @item.update(enabled: false)
+    redirect_to dashboard_items_path,  danger: "Item #{@item.id} has been disabled"
+  end
 
-def activate
-  @item = Item.find(params[:id])
-  @item.update(enabled: true)
-  redirect_to dashboard_items_path, success: "Item #{@item.id} has been enabled"
-end
+  def activate
+    @item = Item.find(params[:id])
+    @item.update(enabled: true)
+    redirect_to dashboard_items_path, success: "Item #{@item.id} has been enabled"
+  end
 
   private
 
@@ -28,3 +43,11 @@ end
     render file: "/public/404" unless current_merchant?
   end
 end
+
+  def item_params
+    params.require(:item).permit(:item_name,
+                                 :description,
+                                 :image_url,
+                                 :current_price,
+                                 :inventory).merge(user_id: current_user.id)
+  end
