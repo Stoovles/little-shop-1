cafcd2973298853840f56de49a0c9bbc936d1507
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-08 12:55:03 -0600

    Adds tests for welcome page

---
+++ spec/features/welcome_page_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "As any user on the welcome page" do
+  before :each do
+    visit root_path
+  end
+  describe "website description" do
+    it "should show information about the website" do
+
+    end
+
+    it "should have a login or register section" do
+      within "welcome-login" do
+        expect(page).to have_content("Log In")
+        expect(page).to have_link("Log In")
+        expect(page).to have_link("Not a Member? Register Here")
+        click_link "Not a Member? Register Here"
+        expect(current_path).to eq(new_user_path)
+      end
+    end
+
+    it "should have an items/merchants section" do
+      within "welcome-nav-section" do
+        expect(page).to have_content("Browse")
+        expect(page).to have_link("All Items")
+        expect(page).to have_link("All Merchants")
+      end
+    end
+  end
+
+  describe "creators description" do
+    it "should have a blurb about us and the project" do
+      expect(page).to have_content("About Creators")
+      expect(page).to have_link("Jeremy Bennett's Github")
+      expect(page).to have_link("Ethan Grab's Github")
+      expect(page).to have_link("Carrie Walsh's Github")
+      expect(page).to have_link("Matt Weiss's Github")
+    end
+  end
+end
