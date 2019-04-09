3213678c54730506ba3f86ce5dd9675235d50f0f
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-09 13:05:43 -0600

    Adds expect data to top state/city overall tests

--- spec/models/merchant_stats_spec.rb
+++ spec/models/merchant_stats_spec.rb
@@ -10,10 +10,14 @@ RSpec.describe 'when we visit a merchant show page' do
     @u21 = User.create(name: "Abby Stedell",street_address: "0383 Shasta Circle",city: "Miami",state: "California",zip_code: "33127",email_address: "astedellk@yelp.com",password:"zVstAHzK", enabled: true, role:0)
 
     @umerch = User.create(name: "Ondrea Chadburn",street_address: "6149 Pine View Alley",city: "Wichita Falls",state: "Texas",zip_code: "76301",email_address: "ochadburn0@washingtonpost.com",password:"EKLr4gmM44", enabled: true, role:1)
+    @umerch2 = User.create(name: "Raff Faust",street_address: "066 Debs Place",city: "El Paso",state: "Texas",zip_code: "79936",email_address: "rfaust1@naver.com",password:"ZCoxai", enabled: true, role:1)
 
     @i8 = @umerch.items.create(item_name: "Four Roses Single Barrel",image_url: "https://www.totalwine.com/media/sys_master/twmmedia/h2d/h6e/11152774365214.png",current_price: 43.0,inventory: 30, description:"Dried spice, pear, cocoa, vanilla & maple syrup. Hints of ripe plum & cherries, robust, full body, mellow. Smooth & delicately long.",enabled: true)
     @i15 = @umerch.items.create(item_name: "Jack Daniels Single Barrel Heritage Barrel",image_url: "http://s3.amazonaws.com/mscwordpresscontent/wa/wp-content/uploads/2018/11/Jack-Daniels-Heritage.png",current_price: 65.0,inventory: 33, description:"Tennessee- A delicious and complex whiskey. Barreled in heavy-toast barrels and bottled at 100 proof. Notes of baking spices, vanilla, and toasted oak. Long, lingering finish.",enabled: true)
     @i24 = @umerch.items.create(item_name: "Little Book Chapter 2 'Noe Simple Task'",image_url: "http://s3.amazonaws.com/mscwordpresscontent/wa/wp-content/uploads/2018/11/Little-Book-Noe-Simple.png",current_price: 100.0,inventory: 40, description:"Blend of whiskeys from Jim Beam and Canadian distilleries owned by Beam Suntory",enabled: true)
+    @i12 = @umerch2.items.create(item_name: "Pappy Van Winkle 23 Year",image_url: "https://www.buffalotracedistillery.com/sites/default/files/PVW-23yr.png",current_price: 270.0,inventory: 35, description:"Early aromas of caramel and cream with soft notes of oak, nuts and leather. Features a sweet taste composed of vanilla, maple and honey joined by complimentary flavors of citrus and spice. Has a long lasting oaky finish, along with many of the early flavors.",enabled: true)
+    @i13 = @umerch2.items.create(item_name: "Wild Turkey 101",image_url: "https://www.totalwine.com/dynamic/490x/media/sys_master/twmmedia/hc4/h94/9923597533214.png",current_price: 19.0,inventory: 28, description:"Corn and waxy honey aroma, with a deep chunk of earthy rye behind it. Not overly sweet in the mouth. This is lean, almost leathery, and leads to a long, strong finish, where a spike of mint appears. Definitely more than the barshot Bourbon it's often relegated to.",enabled: true)
+
 
     @o1 = @u16.orders.create(status: 2)
     @o2 = @u16.orders.create(status: 2)
@@ -24,6 +28,10 @@ RSpec.describe 'when we visit a merchant show page' do
     @o7 = @u19.orders.create(status: 2)
     @o8 = @u20.orders.create(status: 2)
     @o9 = @u21.orders.create(status: 2)
+    @o10 = @u21.orders.create(status: 2)
+    @o11 = @u21.orders.create(status: 2)
+    @o12 = @u21.orders.create(status: 2)
+
 
     @oi1 = OrderItem.create(order_id: @o1.id,item_id: @i8.id, quantity: 4,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
     @oi2 = OrderItem.create(order_id: @o2.id,item_id: @i15.id, quantity: 6,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
@@ -34,27 +42,30 @@ RSpec.describe 'when we visit a merchant show page' do
     @oi7 = OrderItem.create(order_id: @o7.id,item_id: @i8.id, quantity: 16,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
     @oi8 = OrderItem.create(order_id: @o8.id,item_id: @i15.id, quantity: 18,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
     @oi9 = OrderItem.create(order_id: @o9.id,item_id: @i24.id, quantity: 20,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
+    @oi10 = OrderItem.create(order_id: @o10.id,item_id: @i12.id, quantity: 16,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
+    @oi11 = OrderItem.create(order_id: @o11.id,item_id: @i12.id, quantity: 18,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
+    @oi12 = OrderItem.create(order_id: @o12.id,item_id: @i13.id, quantity: 200,fulfilled: true,order_price: 10.0,created_at: "2018-04-05 11:50:20",updated_at: "2018-04-13 13:08:43")
   end
 
   describe 'self.top_three_states' do
     it 'lists top 3 states by quantity sold' do
-      expect(User.top_three_states.first.state).to eq("California")
-      expect(User.top_three_states.first.sum).to eq(34)
-      expect(User.top_three_states.second.state).to eq("Nevada")
-      expect(User.top_three_states.second.sum).to eq(30)
-      expect(User.top_three_states.third.state).to eq("Pennsylvania")
-      expect(User.top_three_states.third.sum).to eq(18)
+      expect(User.top_three_states(@umerch).first.state).to eq("California")
+      expect(User.top_three_states(@umerch).first.sum).to eq(34)
+      expect(User.top_three_states(@umerch).second.state).to eq("Nevada")
+      expect(User.top_three_states(@umerch).second.sum).to eq(30)
+      expect(User.top_three_states(@umerch).third.state).to eq("Pennsylvania")
+      expect(User.top_three_states(@umerch).third.sum).to eq(18)
     end
   end
 
   describe 'self.top_three_city_states' do
     it 'lists top 3 city, states by quantity sold' do
-      expect(User.top_three_city_states.first.citystate).to eq("Fresno, Nevada")
-      expect(User.top_three_city_states.first.sum).to eq(30)
-      expect(User.top_three_city_states.second.citystate).to eq("Miami, California")
-      expect(User.top_three_city_states.second.sum).to eq(20)
-      expect(User.top_three_city_states.third.citystate).to eq("Harrisburg, Pennsylvania")
-      expect(User.top_three_city_states.third.sum).to eq(18)
+      expect(User.top_three_city_states(@umerch).first.citystate).to eq("Fresno, Nevada")
+      expect(User.top_three_city_states(@umerch).first.sum).to eq(30)
+      expect(User.top_three_city_states(@umerch).second.citystate).to eq("Miami, California")
+      expect(User.top_three_city_states(@umerch).second.sum).to eq(20)
+      expect(User.top_three_city_states(@umerch).third.citystate).to eq("Harrisburg, Pennsylvania")
+      expect(User.top_three_city_states(@umerch).third.sum).to eq(18)
     end
   end
 
@@ -84,23 +95,23 @@ RSpec.describe 'when we visit a merchant show page' do
 
   describe "self.top_three_states_overall" do
     it "lists top 3 states overall by order count" do
-      expect(User.top_three_states_overall.first.state).to eq("")
-      expect(User.top_three_states_overall.first.sum).to eq()
-      expect(User.top_three_states_overall.second.state).to eq("")
-      expect(User.top_three_states_overall.second.sum).to eq()
-      expect(User.top_three_states_overall.third.state).to eq("")
-      expect(User.top_three_states_overall.third.sum).to eq()
+      expect(User.top_three_states_overall.first.state).to eq("California")
+      expect(User.top_three_states_overall.first.sum).to eq(5)
+      expect(User.top_three_states_overall.second.state).to eq("Nevada")
+      expect(User.top_three_states_overall.second.sum).to eq(3)
+      expect(User.top_three_states_overall.third.state).to eq("Missouri")
+      expect(User.top_three_states_overall.third.sum).to eq(2)
     end
   end
 
   describe "self.top_three_city_states_overall" do
     xit "lists top 3 city,states overall by order count" do
-      expect(User.top_three_city_states_overall.first.citystate).to eq("")
-      expect(User.top_three_city_states_overall.first.sum).to eq()
-      expect(User.top_three_city_states_overall.second.citystate).to eq("")
-      expect(User.top_three_city_states_overall.second.sum).to eq()
-      expect(User.top_three_city_states_overall.third.citystate).to eq("")
-      expect(User.top_three_city_states_overall.third.sum).to eq()
+      expect(User.top_three_city_states_overall.first.citystate).to eq("Miami, California")
+      expect(User.top_three_city_states_overall.first.sum).to eq(4)
+      expect(User.top_three_city_states_overall.second.citystate).to eq("Fresno, Nevaeda")
+      expect(User.top_three_city_states_overall.second.sum).to eq(3)
+      expect(User.top_three_city_states_overall.third.citystate).to eq("Saint Louis, Missouri")
+      expect(User.top_three_city_states_overall.third.sum).to eq(2)
     end
   end
 
