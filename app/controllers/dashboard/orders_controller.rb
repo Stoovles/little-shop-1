edfa6019ca979f4773e5cf86b8282e7575680499
Matt Weiss <weiss.matt@outlook.com>
2019-04-09 09:31:26 -0600

    Moves update_inventory method down to model from controller.
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>
    Co-authored-by: Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -7,8 +7,7 @@ class Dashboard::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @item = Item.find(params[:item_id])
     @order.order_items.where(item_id: @item).first.update(fulfilled: true)
-    new_inventory = @item.inventory - @order.order_items.where(item_id: @item).first.quantity #convert this to instance method in model
-    @item.update(inventory: new_inventory)
+    @item.update_inventory(@order)
     flash.notice = "You have fulfilled #{@item.item_name}"
     redirect_to dashboard_order_path(@order)
   end
