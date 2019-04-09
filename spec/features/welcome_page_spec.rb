5aa53ee5814b24d342f31a17c8728e369eb2ae7e
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-08 19:58:29 -0600

    Adds more to welcome page

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -10,14 +10,23 @@ RSpec.describe "As any user on the welcome page" do
       expect(page).to have_css(".about-site")
 
     end
+    describe "as a visitor" do
+      it "should have a login or register section" do
+        within ".welcome-login" do
+          expect(page).to have_content("Log In")
+          expect(page).to have_button("Log Me In")
+          expect(page).to have_link("Not a Member? Register Here")
+          click_link "Not a Member? Register Here"
+          expect(current_path).to eq(new_user_path)
+        end
+      end
+    end
 
-    it "should have a login or register section" do
-      within ".welcome-login" do
-        expect(page).to have_content("Log In")
-        expect(page).to have_button("Log Me In")
-        expect(page).to have_link("Not a Member? Register Here")
-        click_link "Not a Member? Register Here"
-        expect(current_path).to eq(new_user_path)
+    describe "as a logged in user/merchant/admin" do
+      it "should have a welcome section with logout button" do
+        user =
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        expect(page).to have_content("Welcome, #{user.name}")
       end
     end
 
