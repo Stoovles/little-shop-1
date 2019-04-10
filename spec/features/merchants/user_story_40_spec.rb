11859fa6977e60e0a21f2a8e89dfd6ecc22983f3
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-04-10 11:53:37 -0600

    refactors top 3 merchants test to 2 expectations

--- spec/features/merchants/user_story_40_spec.rb
+++ spec/features/merchants/user_story_40_spec.rb
@@ -53,7 +53,8 @@ RSpec.describe 'as a visitor when I visit the merchant index page' do
 
   describe "in the statistics section" do
     it "should have the top 3 merchnts" do
-      expect(page).to have_content("Top 3 Merchants: #{@umerch2.name} - $4,330.00 #{@umerch.name} - $1,080.00")
+      expect(page).to have_content("Top 3 Merchants:")
+      expect(page).to have_content("#{@umerch2.name} - $4,330.00 #{@umerch.name} - $1,080.00")
     end
 
     it "should have the 3 fastest merchants" do
