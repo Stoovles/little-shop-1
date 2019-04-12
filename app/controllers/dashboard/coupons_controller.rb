2cef6bcf118f4f3c93987777e2ce6422ee48ea02
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-12 11:02:46 -0600

    Adds tests for coupons show page

--- app/controllers/dashboard/coupons_controller.rb
+++ app/controllers/dashboard/coupons_controller.rb
@@ -2,4 +2,8 @@ class Dashboard::CouponsController < ApplicationController
   def index
     @coupons = current_user.coupons
   end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
 end
--- spec/features/dashboard/coupons_spec.rb
+++ spec/features/dashboard/coupons_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "On the merchant coupons index" do
+RSpec.describe "merchant coupons" do
   before :each do
     @u1 = User.create(name: "Ondrea Chadburn",street_address: "6149 Pine View Alley",city: "Wichita Falls",state: "Texas",zip_code: "76301",email_address: "ochadburn0@washingtonpost.com",password:"EKLr4gmM44", enabled: true, role:1)
 
@@ -9,13 +9,42 @@ RSpec.describe "On the merchant coupons index" do
     @c3 = @u1.coupons.create(name: "HOLIDAY15PERCENT",	discount: 1, amount: 15, active: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@u1)
-    visit dashboard_coupons_path
   end
 
-  it "should show my coupons" do
-    expect(page).to have_css(".coupon-card", count: 3)
-    expect(page).to have_link("#{@c1.name}")
+  describe "on the merchant coupons index" do
+    it "should show my coupons" do
+      visit dashboard_coupons_path
+      expect(page).to have_css(".coupon-card", count: 3)
+      expect(page).to have_link("#{@c1.name}")
+      click_link "#{@c1.name}"
+      expect(current_path).to eq(dashboard_coupon_path(@c1))
+    end
   end
+
+  describe "on the coupon show page" do
+    it "should show information about the coupon" do
+      visit dashboard_coupon_path(@c1)
+      expect(page).to have_content(@c1.name)
+      expect(page).to have_content("$#{@c1.amount}.00 off")
+      expect(page).to have_content("#{@c1.active}")
+
+      visit dashboard_coupon_path(@c3)
+      expect(page).to have_content("#{@c3.amount}% off")
+    end
+
+    it "should show a list of all users who have used the coupon" do
+      visit dashboard_coupon_path(@c1)
+    end
+
+    xit "should have an activate button for disactivated coupons" do
+
+    end
+
+    xit "should have a disactivate button for active coupons" do
+
+    end
+  end
+
 
 
 end
