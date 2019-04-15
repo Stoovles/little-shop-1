4f0ee1cbf4008494b38dba7fded70c92f4b7d398
Ethan Grab <ethangrab@gmail.com>
2019-04-15 11:30:44 -0600

    modified create if coupon applied to order

--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -1,17 +1,38 @@
 class Profile::OrdersController < ApplicationController
   def index
     @orders = current_user.orders
+    @coupon = session[:coupon]
   end
 
   def create
-    new_order = current_user.orders.create(status: "pending")
-    session[:cart].each do |item_id, quantity|
-      order_item_price = Item.find(item_id).current_price * quantity
-      OrderItem.create(order_id: new_order.id,
-                        item_id: item_id,
-                       quantity: quantity,
-                    order_price: order_item_price,)
+    if session[:coupon]
+      #if coupon is applied
+      new_order = current_user.orders.create(status: "pending", coupon_id: session[:coupon]["id"])
+      session[:cart].each do |item_id, quantity|
+        item = Item.find(item_id)
+        order_item_price = @cart.discount_price(item, session[:coupon]) * quantity
+        OrderItem.create(order_id: new_order.id,
+                          item_id: item_id,
+                         quantity: quantity,
+                      order_price: order_item_price)
+
+      end
+      #update coupon to used: true
+      Coupon.find(session[:coupon]["id"]).update(used: true)
+    else
+      #if no coupon is applied
+      new_order = current_user.orders.create(status: "pending")
+      session[:cart].each do |item_id, quantity|
+        order_item_price = Item.find(item_id).current_price * quantity
+        OrderItem.create(order_id: new_order.id,
+                          item_id: item_id,
+                         quantity: quantity,
+                      order_price: order_item_price)
+      end
+
     end
+
+
     session.delete(:cart)
     redirect_to profile_orders_path, success: 'Your order was successfully created!'
   end
