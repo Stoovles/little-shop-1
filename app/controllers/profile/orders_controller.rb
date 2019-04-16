8ec2da29910028f9b4e651bc00a501ff2217bbf9
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-15 20:34:02 -0600

    Adds tests for dollar coupons

--- app/controllers/profile/orders_controller.rb
+++ app/controllers/profile/orders_controller.rb
@@ -8,15 +8,15 @@ class Profile::OrdersController < ApplicationController
     @coupon = Coupon.find_by(name: session[:coupon])
     session[:cart].each do |item_id, quantity|
       base_price = Item.find(item_id).current_price
-      if @coupon && @coupon.item_list.pluck(:id).include?(item_id)
+      if @coupon && @coupon.item_list.pluck(:id).include?(item_id.to_i)
         discount = 0
         discount_remainder = @coupon.amount - discount
         if @coupon.discount == "percent"
           order_item_price = Item.find(item_id).current_price * @coupon.percent_off
-        elsif  base_price > @coupon.amount
+        elsif  base_price < @coupon.amount
           order_item_price = Item.find(item_id).current_price - @coupon.amount
           discount_remainder = 0
-        elsif  base_price < @coupon.amount
+        elsif  base_price >= @coupon.amount
           order_item_price = Item.find(item_id).current_price - discount_remainder
           discount_remainder - order_item_price
         end
@@ -49,23 +49,5 @@ class Profile::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order.update(status: 3)
     redirect_to profile_path, danger: "Order #{@order.id} has been cancelled"
-  end
-end
-
-def discount_total(coupon)
-  regular_total = regular_items(coupon).inject(0) {|total, item| total += subtotal(item)}
-  if coupon.discount == "dollar"
-    if (cart_total - coupon.amount) < 0
-      regular_total
-    else
-      cart_total - coupon.amount
-    end
-  elsif coupon.discount == "percent"
-    if coupon.amount >= 100
-      discount_total = discount_items(coupon).inject(0) {|total, item| total += subtotal(item)*((100 - coupon.amount)/100.0)}
-    else
-      discount_total = 0
-    end
-    discount_total + regular_total
   end
 end
--- spec/features/users/coupon_order_spec.rb
+++ spec/features/users/coupon_order_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "cart and coupon methods", type: :feature do
+RSpec.describe "order creation with coupon", type: :feature do
   before :each do
     @umerch = User.create(name: "Ondrea Chadburn",street_address: "6149 Pine View Alley",city: "Wichita Falls",state: "Texas",zip_code: "76301",email_address: "ochadburn0@washingtonpost.com",password:"EKLr4gmM44", enabled: true, role:1)
     @umerch2 = User.create(name: "Raff Faust",street_address: "066 Debs Place",city: "El Paso",state: "Texas",zip_code: "79936",email_address: "rfaust1@naver.com",password:"ZCoxai", enabled: true, role:1)
@@ -10,7 +10,7 @@ RSpec.describe "cart and coupon methods", type: :feature do
 
     @i1 = @umerch.items.create(item_name: "W.L. Weller Special Reserve",image_url: "http://www.buffalotracedistillery.com/sites/default/files/weller%20special%20reserve%20brand%20page%5B1%5D.png",current_price: 20.0,inventory: 4, description:"A sweet nose with a presence of caramel. Tasting notes of honey, butterscotch, and a soft woodiness. It's smooth, delicate and calm. Features a smooth finish with a sweet honeysuckle flair.",enabled: true)
 
-    @i2 = @umerch.items.create(item_name: "W.L. Weller C.Y.P.B.",image_url: "http://www.buffalotracedistillery.com/sites/default/files/Weller_CYPB_750ml_front_LoRes.png",current_price: 35.0,inventory: 30, description:"A light aroma with citrus and oak on the nose. The palate is well rounded and balanced, with a medium-long finish and hints of vanilla.",enabled: false)
+    @i2 = @umerch.items.create(item_name: "W.L. Weller C.Y.P.B.",image_url: "http://www.buffalotracedistillery.com/sites/default/files/Weller_CYPB_750ml_front_LoRes.png",current_price: 35.0,inventory: 30, description:"A light aroma with citrus and oak on the nose. The palate is well rounded and balanced, with a medium-long finish and hints of vanilla.",enabled: true)
 
     @i19 = @umerch2.items.create(item_name: "Armorik Double Maturation",image_url: "http://s3.amazonaws.com/mscwordpresscontent/wa/wp-content/uploads/2018/11/Armorik-Double.png",current_price: 60.0,inventory: 33, description:"French Single malt that takes a slightly different route than it's Irish and Scottish cousins and uses new charred oak barrels instead of the more common ex-bourbon barrels.",enabled: true)
 
@@ -20,6 +20,7 @@ RSpec.describe "cart and coupon methods", type: :feature do
     @c2 = @umerch.coupons.create(name: "SUMMER20OFF",	discount: 0, amount: 20, active: 0)
     @c3 = @umerch2.coupons.create(name: "HOLIDAY15PERCENT",	discount: 1, amount: 15, active: 1)
     @c4 = @umerch2.coupons.create(name: "15PERCENT",	discount: 1, amount: 15, active: 0)
+    @c5 = @umerch2.coupons.create(name: "15OFF",	discount: 0, amount: 15, active: 0)
     @u1.coupons << @c1
 
     visit login_path
@@ -33,61 +34,31 @@ RSpec.describe "cart and coupon methods", type: :feature do
     click_button "Add to Cart"
   end
 
-  describe "coupon functionality" do
-    it "does not allow a visitor to add a coupon" do
-      click_link "Log Out"
-      visit item_path(@i2)
-      click_button "Add to Cart"
-      visit cart_path
-      within ".cart-container" do
-        expect(page).to_not have_button('Add Coupon')
+
+    describe "orders with dollar off coupon" do
+      it "should link to the order create with the dollar coupon" do
+        visit cart_path
+        within ".cart-container" do
+          fill_in "Coupon code", with: "#{@c2.name}"
+          click_button "Add Coupon"
+        end
+        click_button "Check Out"
+        click_link "Order ID:"
+        expect(page).to have_content("Coupon: #{@c2.name}")
+        expect(page).to have_content("Total: $75.00")
       end
     end
 
-    it "allows a logged-in user to add a coupon" do
-      visit cart_path
-      within ".cart-container" do
-        fill_in "Coupon code", with: "example"
-        expect(page).to have_button('Add Coupon')
+    describe "orders with greater than item dollar off coupon" do
+      it "should create the order with the percent coupon" do
+        visit cart_path
+        within ".cart-container" do
+          fill_in "Coupon code", with: "#{@c4.name}"
+          click_button "Add Coupon"
+        end
+        click_button "Check Out"
+        click_link "Order ID:"
+        expect(page).to have_content("Total: $86.00")
       end
     end
-
-    it "does not allow a non-existing coupon to be used" do
-      visit cart_path
-      within ".cart-container" do
-        fill_in "Coupon code", with: "example"
-        click_button "Add Coupon"
-      end
-      expect(page).to have_content("Coupon does not exist")
-    end
-
-    it "does not allow a disabled coupon to be used" do
-      visit cart_path
-      within ".cart-container" do
-        fill_in "Coupon code", with: "#{@c3.name}"
-        click_button "Add Coupon"
-      end
-      expect(page).to have_content("Coupon does not exist")
-    end
-
-    it "does not allow an already used coupon to be used" do
-      visit cart_path
-      within ".cart-container" do
-        fill_in "Coupon code", with: "#{@c1.name}"
-        click_button "Add Coupon"
-      end
-      expect(page).to have_content("Coupon has already been used")
-    end
-
-    it "displays a discounted total if user uses a coupon" do
-      visit cart_path
-
-      within ".cart-container" do
-        fill_in "Coupon code", with: "#{@c2.name}"
-        click_button "Add Coupon"
-      end
-      expect(page).to have_content("#{@c2.name} has been added")
-      expect(page).to have_content("Discount Total: $75.00")
-    end
-  end
 end
